413e34f922d906e4c1daab5f757174e75d3a3fb4
João Lucas Alvarez <joao.lucas.c.alvarez@gmail.com>
2019-07-06 16:42:08 -0300

    a

--- app/platforms/ios/www/cordova_plugins.js
+++ app/platforms/ios/www/cordova_plugins.js
@@ -236,11 +236,20 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
       "clobbers": [
         "window.StatusBar"
       ]
+    },
+    {
+      "id": "cordova-plugin-navigationbar-color.navigationbar",
+      "file": "plugins/cordova-plugin-navigationbar-color/www/navigationbar.js",
+      "pluginId": "cordova-plugin-navigationbar-color",
+      "clobbers": [
+        "window.NavigationBar"
+      ]
     }
   ];
   module.exports.metadata = {
     "cordova-plugin-googlemaps": "2.6.2",
     "cordova-plugin-whitelist": "1.3.3",
-    "cordova-plugin-statusbar": "2.4.2"
+    "cordova-plugin-statusbar": "2.4.2",
+    "cordova-plugin-navigationbar-color": "0.0.8"
   };
 });
