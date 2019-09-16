509cc054af174809166da70975fed6cc8ac93136
Lock-n <30930947+Lock-n@users.noreply.github.com>
2019-09-16 12:29:16 -0300

    Changes to database structure

---
+++ app/www/js/database_interaction.js
@@ -0,0 +1,277 @@
+var db;
+
+document.addEventListener("deviceready", function() {
+  if (cordova.platformId == 'android') {
+    StatusBar.overlaysWebView(true);
+    StatusBar.backgroundColorByHexString('#33000000');
+    db = window.sqlitePlugin.openDatabase({
+      name: 'occurrences.db',
+      location: 'default',
+      androidDatabaseProvider: 'system'
+    }, function() {},
+    function (error) {
+        console.log('Open database ERROR: ' + JSON.stringify(error));
+    });
+  }
+  else {
+    db = window.sqlitePlugin.openDatabase({
+      name: 'occurrences.db',
+      location: 'default'
+    },
+    function() {},
+    function (error) {
+        console.log('Open database ERROR: ' + JSON.stringify(error));
+    });
+  }
+});
+
+var occurrences_jsons = new Array ();
+var map_state = MARKERS_STATE;
+var occurrences_within_view = [];
+
+function getOccurrencesWithinView(result_set) {
+  occurrences_within_view = [];
+
+  for (var i = 0; i < result_set.rows.length; i++) {
+    occurrences_within_view.push(result_set.rows.item(i));
+  }
+
+  switch (map_state) {
+    case MARKERS_STATE:
+      to_marker();
+      break;
+    case HEATMAP_STATE:
+      to_heatmap();
+      break;
+  }
+}
+
+function map_heatmap_with_result_set (result_set) {
+  data = [];
+
+  for (var i = 0; i < result_set.length; i++) {
+    data.push([result_set[i].LATITUDE, result_set[i].LONGITUDE]); // TODO Check if it's float or string
+  }
+
+  map_global.addHeatmap({
+    data: data,
+    radius: 20
+  });
+}
+
+function map_marker_with_result_set (result_set) {
+  for (var i = 0; i < result_set.length; i++) {
+      map_global.addMarker({
+        position: {lat:result_set[i].LATITUDE, lng:result_set[i].LONGITUDE},
+        title: result_set[i].LINKED_NATURE,
+        icon: {
+          url: "./icons/arma.png", // TODO Make images to each type
+          size: {
+            width: 56,
+            height: 56
+          },
+          anchor: {x: 23,y: 46}
+        }
+      });
+  }
+}
+
+function addOccurrence(occurrence) {
+  db.transaction(function (tx) {
+
+      var query = "INSERT INTO occurrences (BO_YEAR, BO_NUMBER, BO_BEGIN_TIME, BO_EMISSION_TIME, DATE, PERIOD, IS_FLAGRANT, ADDRESS_STREET, ADDRESS_NUMBER, ADDRESS_DISTRICT, ADDRESS_CITY, ADDRESS_STATE, LATITUDE, LONGITUDE, PLACE_DESCRIPTION, POLICE_STATION_NAME, POLICE_STATION_CIRCUMSCRIPTION, RUBRIC, FATAL_VICTIM, PERSON_SEX, PERSON_AGE, PERSON_SKIN_COLOR, LINKED_NATURE) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);";
+
+      var occurrence_array = [
+        occurrence.BO_YEAR, occurrence.BO_NUMBER, 
+        occurrence.BO_BEGIN_TIME, occurrence.BO_EMISSION_TIME, 
+        occurrence.DATE, occurrence.PERIOD,
+        occurrence.IS_FLAGRANT, 
+        occurrence.ADDRESS_STREET, occurrence.ADDRESS_NUMBER, 
+        occurrence.ADDRESS_DISTRICT, occurrence.ADDRESS_CITY, 
+        occurrence.ADDRESS_STATE, occurrence.LATITUDE, 
+        occurrence.LONGITUDE, occurrence.PLACE_DESCRIPTION, 
+        occurrence.POLICE_STATION_NAME, occurrence.POLICE_STATION_CIRCUMSCRIPTION,
+        occurrence.RUBRIC, 
+        occurrence.FATAL_VICTIM, occurrence.PERSON_SEX, 
+        occurrence.PERSON_AGE, occurrence.PERSON_SKIN_COLOR, 
+        occurrence.LINKED_NATURE
+      ];
+
+      tx.executeSql(query, occurrence_array, function(tx, res) {
+          console.log("insertId: " + res.insertId);
+          console.log("rowsAffected: " + res.rowsAffected + " -- should be 1");
+      },
+      function(tx, error) {
+          console.log('INSERT error: ' + error.message);
+      });
+
+      query = "INSERT INTO occurrences_index (minLng, maxLng, minLat, maxLat) VALUES (?,?,?,?);";
+      
+      occurrence_array = [
+        occurrence.LONGITUDE, occurrence.LONGITUDE, occurrence.LATITUDE, occurrence.LATITUDE
+      ];
+
+      tx.executeSql(query, occurrence_array, function(tx, res) {
+          console.log("insertId: " + res.insertId);
+          console.log("rowsAffected: " + res.rowsAffected + " -- should be 4");
+      },
+      function(tx, error) {
+          console.log('INSERT error: ' + error.message);
+      });
+  }, function(error) {
+      console.log('transaction error: ' + error.message);
+  }, function() {
+      console.log('transaction ok');
+  });
+}
+
+function getOccurrencesWithinRectangle(maxLng, minLng, maxLat, minLat) {
+  db.transaction(function (tx) {
+      var query = "SELECT occurrences.BO_YEAR, occurrences.BO_NUMBER, occurrences.BO_BEGIN_TIME, occurrences.BO_EMISSION_TIME, occurrences.DATE, occurrences.PERIOD, occurrences.IS_FLAGRANT, occurrences.ADDRESS_STREET, occurrences.ADDRESS_NUMBER, occurrences.ADDRESS_DISTRICT, occurrences.ADDRESS_CITY, occurrences.ADDRESS_STATE, occurrences.LATITUDE, occurrences.LONGITUDE, occurrences.PLACE_DESCRIPTION, occurrences.POLICE_STATION_NAME, occurrences.POLICE_STATION_CIRCUMSCRIPTION, occurrences.RUBRIC, occurrences.FATAL_VICTIM, occurrences.PERSON_SEX, occurrences.PERSON_AGE, occurrences.PERSON_SKIN_COLOR, occurrences.LINKED_NATURE FROM occurrences, occurrences_index WHERE occurrences.ID=occurrences_index.ID AND occurrences_index.maxLng<=(?) AND occurrences_index.minLng>=(?) AND occurrences_index.maxLat<=(?) AND occurrences_index.minLat>=(?);";
+
+      tx.executeSql(query, [maxLng, minLng, maxLat, minLat], function (tx, resultSet) {
+        getOccurrencesWithinView(resultSet);
+      },
+      function (tx, error) {
+          console.log('SELECT error: ' + error.message);
+      });
+  }, function (error) {
+      console.log('transaction error: ' + error.message);
+  }, function () {
+      console.log('transaction ok');
+  });
+}
+
+function insert_json_in_db (dir_json) {
+  function convert_date_format(date) {
+    // date: DD/MM/YYYY HH:MM
+    var return_string = "";
+    var parts = date.split(" ");
+    var first_half = parts[0].split("/");
+
+    var year = first_half[2];
+    var month = first_half[1];
+    var day = first_half[0];
+
+    return_string = year + "-" + month + "-" + day;
+
+    if (parts.length == 2) {
+      /*var second_half = parts[1].split(":");
+
+      var hours = second_half[0];
+      var minutes = second_half[1];*/
+
+      return_string = return_string + " " + parts[1]; // hours + ":" + minutes
+    }
+
+    return return_string;
+  }
+
+  function convert_period(period) {
+    period = period.toUpperCase();
+
+    switch (period) {
+      case "PELA MANHÃ":
+        return 'M';
+      case "DE MADRUGADA":
+        return 'D';
+      case "A TARDE":
+        return 'N';
+      case "A NOITE":
+        return 'E';
+      case "EM HORA INCERTA":
+        return 'U';
+    }
+  }
+
+  function convert_yes_no_to_bool(yes_no) {
+    yes_no = yes_no[0].toUpperCase();
+
+    return yes_no == "S";
+  }
+
+  function convert_person_sex(sex) {
+    switch (sex[0].toUpperCase()) {
+      case "M":
+        return "M";
+      case "F":
+        return "F";
+      default:
+        return "U";
+    }
+  }
+
+  function convert_skin_color(skin_color) {
+    skin_color = skin_color.toLowerCase().replace("ã", "a");
+
+    switch (skin_color) {
+      case "branca":
+      case "branco":
+        return 'W';
+      case "negra":
+      case "negro":
+      case "preta":
+      case "preto":
+        return 'B';
+      case "parda":
+      case "pardo":
+        return 'P';
+      case "outros":
+      case "outro":
+        return 'O';
+      case "nao informada":
+      case "nao informado":
+        return 'N';  
+    }
+  }
+
+  $.getJSON( dir_json, function( data ) {
+    $.each( data, function(key,value) {
+      if (value.LATITUDE != "") {
+        var occurrence = {
+          BO_YEAR: (value.ANO_BO != "" ? parseInt(value.ANO_BO) : null),
+          BO_NUMBER: (value.NUM_BO != "" ? parseInt(value.NUM_BO) : null),
+          BO_BEGIN_TIME: (value.BO_INICIADO != "" ? convert_date_format(value.BO_INICIADO) : null),
+          BO_EMISSION_TIME: (value.BO_EMITIDO != "" ? convert_date_format(value.BO_EMITIDO) : null),
+          DATE: (value.DATAOCORRENCIA != "" ? convert_date_format(value.DATAOCORRENCIA) : null),
+          PERIOD: (value.PERIDOOCORRENCIA != "" ? convert_period(value.PERIDOOCORRENCIA) : null),
+          IS_FLAGRANT: (value.FLAGRANTE != "" ? convert_yes_no_to_bool(value.FLAGRANTE) : null),
+          ADDRESS_STREET: (value.LOGRADOURO != "" ? value.LOGRADOURO : null),
+          ADDRESS_NUMBER: (value.NUMERO != "" ? parseInt(value.NUMERO) : null),
+          ADDRESS_DISTRICT: (value.BAIRRO != "" ? value.BAIRRO : null),
+          ADDRESS_CITY: (value.CIDADE != "" ? value.CIDADE : null),
+          ADDRESS_STATE: (value.UF != "" ? value.UF : null),
+          LATITUDE: (value.LATITUDE != "" ? parseFloat(value.LATITUDE.replace(",",".")) : null),
+          LONGITUDE: (value.LONGITUDE != "" ? parseFloat(value.LONGITUDE.replace(",",".")) : null),
+          PLACE_DESCRIPTION: (value.DESCRICAOLOCAL != "" ? value.DESCRICAOLOCAL : null),
+          POLICE_STATION_NAME: (value.DELEGACIA_NOME != "" ? value.DELEGACIA_NOME : null),
+          POLICE_STATION_CIRCUMSCRIPTION: (value.DELEGACIA_CIRCUNSCRICAO != "" ? value.DELEGACIA_CIRCUNSCRICAO : null),
+          RUBRIC: (value.RUBRICA != "" ? value.RUBRICA : null),
+          FATAL_VICTIM: (value.VITIMAFATAL != "" ? convert_yes_no_to_bool(value.VITIMAFATAL) : null),
+          PERSON_SEX: (value.SEXO != "" ? convert_person_sex(value.SEXO) : null),
+          PERSON_AGE: (value.IDADE != "" ? parseInt(value.IDADE) : null),
+          PERSON_SKIN_COLOR: (value.CORCUTIS != "" ? convert_skin_color(value.CORCUTIS) : null),
+          LINKED_NATURE: (value.NATUREZAVINCULADA != "" ? value.NATUREZAVINCULADA : null)
+        }
+
+        addOccurrence(occurrence);
+      } else {
+        console.log("Sem coordenadas");
+      }
+    });
+  });
+}
+
+var result_set_eq;
+function execute_query(query) {
+  db.transaction(function (tx) {
+    tx.executeSql(query, [], function(tx, resultSet) {
+      result_set_eq = resultSet;
+      console.log(resultSet);
+    });
+}, function (error) {
+    console.log('transaction error: ' + error.message);
+}, function () {
+    console.log('transaction ok');
+});
+}
--- app/www/js/database_interaction_old.js
+++ app/www/js/database_interaction_old.js
@@ -1,8 +1,3 @@
-function setVar (key, value) {
-  window.localStorage.setItem(key, value);
-}
-
-var map_global;
 var db;
 
 document.addEventListener("deviceready", function() {
@@ -28,164 +23,11 @@ document.addEventListener("deviceready", function() {
         console.log('Open database ERROR: ' + JSON.stringify(error));
     });
   }
-
-  $('#opcoes').sidenav({
-    onOpenStart: function() {
-    setVar('state','side_menu');
-  }, 
-  onCloseEnd: function() {
-    setVar('state','home');
-  }
-  });
-  $('#filtros').modal({
-    onOpenStart: function() {
-    setVar('state','filtros');
-  }, onCloseEnd: function() {
-    setVar('state','home');}});
-  
-  $('#pesquisa-btn-container').click(function(e) {
-    $("#pesquisa-text-container").fadeIn(100);
-    $("#pesquisa-text-container").css({width: (($(window).width()-40) + "px"),right:"20px"});
-    $("#pesquisa-input").delay(500).fadeIn(100);
-    $("#close-btn").delay(500).fadeIn(100);
-    
-    $("#menu-btn-container").fadeOut(200);
-    $("#pesquisa-btn-container").css({display:"none"});
-  }); //onclick="setVar('state','side_menu');"
-
-  $('#close-btn').click(function(e) {
-    $("#search_places").val("");
-    $("#search_places").focus();
-  });
-
-  document.addEventListener("backbutton", onBackKeyDown, false);
-
-  function onBackKeyDown() {
-    var page_state = window.localStorage.getItem("state");
-    switch (page_state) {
-      case "search": {
-        $("#pesquisa-input").fadeOut(100);
-        $("#close-btn").fadeOut(100);
-        $("#pesquisa-text-container").css({width:"56px",right:"23px"});
-        $("#pesquisa-text-container").fadeOut(100);
-
-        $("#menu-btn-container").fadeIn(200);
-        $("#pesquisa-btn-container").css({display:"block"});
-
-        setVar("state","home");
-      }break;
-      
-      case "side_menu": {
-        $("#opcoes").sidenav('close');
-      }break;
-
-      case "filtros": {
-        $("#filtros").modal('close');
-      }break;
-
-      case "home": {
-        navigator.app.exitApp();
-      }break;
-
-      default :{
-        
-      }
-    }
-  }
-
-  var div = document.getElementById("map_canvas");
-  // Create a Google Maps native view under the map_canvas div.
-  var map_style_object;
-  $.getJSON("js/map_style.json", function(result) {
-    map_style_object = result;
-  });
-
-  var map = plugin.google.maps.Map.getMap(div, map_style_object);
-  
-  window.addEventListener('keyboardDidHide', function() {
-    $(':text').blur();
-  });
-
-  function gotoPlace (place) {
-    nativegeocoder.forwardGeocode(gogo, epicfail, place, { useLocale: true, maxResults: 1 });
-
-    function gogo(coordinates) {
-      var firstResult = coordinates[0];
-      map.animateCamera({
-        target: {lat:firstResult.latitude, lng:firstResult.longitude},
-        zoom: 13,
-        tilt: 30,
-        bearing: 0,
-        duration: 1000
-      }, function() {
-      });
-    }
-
-    function epicfail(err) {
-      console.log(err);
-    }
-  }
-  
-  $("#search_places").on("keypress", function(event){
-    if (event.which  == 13) {
-      var place_to_go = $("#search_places").val();
-      gotoPlace(place_to_go);
-      Keyboard.hide();
-      event.preventDefault();
-    }
-  });
-
-  setVar("state","home");
-  map.one(plugin.google.maps.event.MAP_READY, onMapInit);
-  map_global = map;
 });
-
-const HEATMAP = {id: -1, path_img: ""};
-const FURTO_CELULAR = {id: 0, path_img: "./icons/arma.png"};
-const ROUBO_CELULAR = {id: 1, path_img: "./icons/arma.png"};
-const FURTO_VEICULOS = {id: 2, path_img: "./icons/arma.png"};
-const ROUBO_VEICULOS = {id: 3, path_img: "./icons/arma.png"};
-const LATROCINIO = {id: 4, path_img: "./icons/arma.png"};
-const LESAO_CORPORAL_SEGUIDA_DE_MORTE = {id: 5, path_img: "./icons/arma.png"};
-const HOMICIDIO_DOLOSO = {id: 6, path_img: "./icons/arma.png"};
-const MARKERS_STATE = 0;
-const HEATMAP_STATE = 1;
 
 var occurrences_jsons = new Array ();
 var map_state = MARKERS_STATE;
 var occurrences_within_view = [];
-
-function to_marker(/*use_json_or_result_set*/) {
-  //use_json_or_result_set = (typeof use_json_or_result_set !== 'undefined' ? use_json_or_result_set : true);
-  map_state = MARKERS_STATE;
-  map_global.clear();
-
-  /*if (use_json_or_result_set) {
-    $.each( occurrences_jsons, function(key,value) {
-      mapear_marker(value.dir_json, value.tipo);
-    });
-  }
-  else {
-    map_marker_with_result_set(occurrences_within_view);
-  }*/
-  map_marker_with_result_set(occurrences_within_view);
-}
-
-function to_heatmap(/*use_json_or_result_set*/) {
-  //use_json_or_result_set = (typeof use_json_or_result_set !== 'undefined' ? use_json_or_result_set : true);
-  map_state = HEATMAP_STATE;
-  map_global.clear();
-  
-  /*if (use_json_or_result_set) {
-    $.each( occurrences_jsons, function(key,value) {
-      mapear_heatmap(value.dir_json, value.tipo);
-    });
-  }
-  else {
-    map_heatmap_with_result_set(occurrences_within_view);
-  }*/
-  map_heatmap_with_result_set(occurrences_within_view);
-}
 
 function getOccurrencesWithinView(result_set) {
   occurrences_within_view = [];
@@ -204,24 +46,6 @@ function getOccurrencesWithinView(result_set) {
   }
 }
 
-/*function mapear_heatmap (dir_json, tipo) {
-  $.getJSON( dir_json, function( data ) {
-    var heatmapData = [];
-    $.each( data, function(key,value) {
-      if (value.LATITUDE != "") {
-        heatmapData.push([parseFloat(value.LATITUDE.replace(",",".")), parseFloat(value.LONGITUDE.replace(",",".")), 5]);}
-      else
-      {
-        console.log("Sem coordenadas");
-      }});
-
-      map_global.addHeatmap({
-        data: heatmapData,
-        radius: 20
-      });
-    });
-}*/
-
 function map_heatmap_with_result_set (result_set) {
   data = [];
 
@@ -234,29 +58,6 @@ function map_heatmap_with_result_set (result_set) {
     radius: 20
   });
 }
-
-/*function mapear_marker (dir_json, tipo) {
-  $.getJSON( dir_json, function( data ) {
-    $.each( data, function(key,value) {
-      if (value.LATITUDE != "") {
-        map_global.addMarker({
-          position: {lat:parseFloat(value.LATITUDE.replace(",",".")), lng:parseFloat(value.LONGITUDE.replace(",","."))},
-          title: value.RUBRICA,
-          icon: {
-            url: tipo.path_img,
-            size: {
-              width: 56,
-              height: 56
-            },
-            anchor: {x: 23,y: 46}
-          }
-        });
-      } else {
-        console.log("Sem coordenadas");
-      }
-    });
-  });
-}*/
 
 function map_marker_with_result_set (result_set) {
   for (var i = 0; i < result_set.length; i++) {
@@ -536,105 +337,6 @@ function insert_json_in_db (dir_json) {
       }
     });
   });
-}
-
-//mapear_marker("./occurrences/DadosBO_2019_7(ROUBO DE CELULAR).csv_unique.csv.json", ROUBO_CELULAR);
-
-var delta_function = function (zoom) {
-  return 353.306270268435128 * Math.exp(-0.676030142340657 * zoom);
-}
-
-var previous_camera_position;
-var previous_delta;
-var dist = 0.6;
-var zoom_change = 0.05;
-function onMapInit (map) {
-  map.on(plugin.google.maps.event.CAMERA_MOVE_END, function(cameraPosition) {
-    var delta = delta_function(cameraPosition.zoom);
-
-    var moved_camera_considerably = true;
-    var changed_zoom_considerably = true;
-    if (previous_camera_position)
-    {
-      var d_y = (previous_camera_position.target.lat - cameraPosition.target.lat);
-      var d_x = (previous_camera_position.target.lng - cameraPosition.target.lng);
-      var distance = Math.sqrt(d_x * d_x + d_y * d_y);
-
-      //console.log("distance/delta: " + (distance/delta));
-      
-      moved_camera_considerably = ((distance/delta) > dist);
-    }
-    if (previous_delta) {
-      //console.log("delta difference: " + (Math.abs(delta - previous_delta)));
-      changed_zoom_considerably = (Math.abs(delta - previous_delta) > zoom_change);
-    }
-
-    //console.log("moved_camera_considerably: " + moved_camera_considerably + "; changed_zoom_considerably: " + changed_zoom_considerably);
-    if (!moved_camera_considerably && !changed_zoom_considerably)
-      return;
-
-    previous_camera_position = cameraPosition;
-    previous_delta = delta;
-
-    //console.log("zoom: " + cameraPosition.zoom);
-    console.log("delta: " + delta);
-
-    if (delta <= 0.1)
-      getOccurrencesWithinRectangle(cameraPosition.target.lng + delta, cameraPosition.target.lng - delta, cameraPosition.target.lat + delta, cameraPosition.target.lat - delta);
-    else
-      M.toast({html: 'Aumente o zoom para ver as occorrências de uma região.'});
-    /*map_global.addMarker({
-      position: {lat:cameraPosition.target.lat + delta, lng:cameraPosition.target.lng + delta},
-      title: "Test upper right limit of view rectangle",
-      icon: {
-        url: "./icons/transparent_red_circle.png",
-        size: {
-          width: 56,
-          height: 56
-        },
-        anchor: {x: 23,y: 46}
-      }
-    });
-
-    map_global.addMarker({
-      position: {lat:cameraPosition.target.lat - delta, lng:cameraPosition.target.lng - delta},
-      title: "Test lower left limit of view rectangle",
-      icon: {
-        url: "./icons/transparent_red_circle.png",
-        size: {
-          width: 56,
-          height: 56
-        },
-        anchor: {x: 23,y: 46}
-      }
-    });*/
-  });
-
-  /*map.on(plugin.google.maps.event.CAMERA_MOVE_START, function(cameraPosition) {
-    previous_camera_position = cameraPosition;
-  });*/
-
-  map.animateCamera({
-    target: {lat:-22.9064, lng:-47.0616 },
-    zoom: 13,
-    tilt: 1e-20,
-    bearing: 0,
-    duration: 0
-  }, function() {
-  });
-  /*map.getMyLocation(function(location) {
-    map.animateCamera({
-      target: location.latLng,
-      zoom: 13,
-      tilt: 30,
-      bearing: 0,
-      duration: 1000
-    }, function() {
-    });
-  }, function() {
-
-  });*/
-  //mapear_marker("./occurrences/DadosBO_2019_7(ROUBO DE CELULAR).csv_unique.csv.json", ROUBO_CELULAR);
 }
 
 var result_set_eq;
